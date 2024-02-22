db0c051e7f4a6493c5390c54e3e9bdb82bd49066
Rounak Paul <rounakpaul@hotmail.com>
2024-02-22 09:16:42 +0530

    a

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
-get_even_num = lambda x: x * 2
-for i in range(40):
-    print(get_even_num(i))
+# Files in python
+
+# Files are required to handle persistent data
+
+# File modes:
+# r - read, if does not exists throw error
+# w - write and create
+# a - append and create
+# x - create, if exists throw error
+
+# t - text
+# b - binary
+
+file_obj = open("example_file.txt", mode="rt")
+for i in file_obj:
+    print(i)
+
+import os
+
+os.remove("example_file.txt")
+
+
